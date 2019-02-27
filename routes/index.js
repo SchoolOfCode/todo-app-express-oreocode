9bededaed736734f2c558f4d5679ec8a64d51cb1
omar sultani <omarsultani@omars-MacBook-Pro.local>
2019-02-27 16:56:56 +0000

    main.html.css.jssfront end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const PORT = 8080;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
+
+connect("test").then(database => {
+  db = database;
+  applicationCache.addEventListener(PORT, () =>
+    console.log("listening! on ${PORT}")
+  );
+});
