1147f2a4a29afafb1ce011ffdf9b491cbf884a00
jaskaran <jaskaran.sarkaria@googlemail.com>
2019-02-27 16:56:52 +0000

    added connection to db and CRUD functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+let connect = require("../connect");
+// show mardown here
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
+
+connect("Todo").then(database => {
+  db = database;
+});
