220f0182e21656aa42e802a3d92c751a6c9c31d4
ropenfold <stuartroper@gmail.com>
2019-02-27 17:04:00 +0000

    1st commit stuart

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.patch("/", function(req, res, next) {
+  let myData = db.collection("users");
+  myData.patch(req.body, (err, result) => {
+    if (err) throw err;
+  });
 });
 
 module.exports = router;
