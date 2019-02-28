4b6656f0b7796ee053a72e37c0dc2ed3c9b3ec38
omar sultani <omarsultani@omars-MacBook-Pro.local>
2019-02-28 16:22:08 +0000

    linked html to js and fetch

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ connect("Todo").then(database => {
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.sendFile(path.join(__dirname, "..", "main.html"));
 });
 
 module.exports = router;
